bbf59cf774fa2fda89078110786be9021fefafd4
kavin-106 <139790396+kavin-106@users.noreply.github.com>
2023-07-18 21:27:16 +0530

    Add files via upload
    day2

---
+++ week1day2/Mobile.java
@@ -0,0 +1,21 @@
+package week1day2;
+
+public class Mobile {
+public static void main(String[] args) {
+	String mobileBrandName ="Oppo";
+	System.out.println("mobile Brand Name :" + mobileBrandName);
+	char mobileLogo ='O';
+	System.out.println("mobile Logo :" + mobileLogo);
+	short noOfMobilePiece =1;
+	System.out.println("no Of Mobile Piece :" + noOfMobilePiece);
+	int modelNumber =855555455;
+	System.out.println("model Number :" + modelNumber);
+	long mobileImeiNumber =245895654541785L;
+	System.out.println("mobile Imei Number :" + mobileImeiNumber);
+	float mobilePrice =17500.50F;
+	System.out.println("mobile Price :" + mobilePrice);
+	boolean isDamaged =true;
+	System.out.println("is Damaged :" + isDamaged);
+
+}
+}
---
+++ week1day2/TwoWheeler.java
@@ -0,0 +1,19 @@
+package week1day2;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+	int noOfWheels = 2;
+	System.out.println("no Of Wheels :" + noOfWheels);
+	short noOfMirrors = 2;
+	System.out.println("no Of Mirrors :" + noOfMirrors);
+	long chassisNumber =1234567891234567L;
+	System.out.println("chassisNumber :" + chassisNumber);
+	boolean isPunctured =false;
+	System.out.println("is Punctured :" + isPunctured);
+	String bikeName = "Rx";
+	System.out.println("bike Name :" + bikeName);
+	double runningKM = 4000.5;
+	System.out.println("runningKM :" + runningKM);
+
+}
+}
