5d1f7a29b7dc259493181545d1eb0196f21e3e91
kavin-106 <139790396+kavin-106@users.noreply.github.com>
2023-07-24 21:04:47 +0530

    Add files via upload
    week 2 day 1

---
+++ week2day1/FibonacciSeries.java
@@ -0,0 +1,43 @@
+package week2day1;
+
+//public class FibonacciSeries {
+	//public static void main(String[] args) {
+		////0  1  1  2  3  5  8  13  21  34  55  89  144
+		//int firstNum=0;
+		//int secondNum=1;
+		//int thirdNum;
+		//int count = 15;
+		//System.out.println(firstNum);
+		//System.out.println(secondNum);
+		//for(int i = 2;i<=count;i++) {
+			//thirdNum=firstNum+secondNum;
+			//System.out.println(thirdNum);
+			//firstNum = secondNum;
+			//secondNum  = thirdNum;
+		//}
+	//}
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class FibonacciSeries{
+	
+	
+}
---
+++ week2day1/PrimeNumber.java
@@ -0,0 +1,13 @@
+package week2day1;
+
+public class PrimeNumber {
+public static void main(String[] args) {
+	int a=11;
+	if (a%2==0||a%5==0) {
+		System.out.println("It is not a primeNumber");
+	}
+	else {
+		System.out.println("It is a primeNumber");
+	}
+}
+}
